e6aea5e4dda62d8b935707fb0233400d75746247
Viral Patel <viralpatel508@gmail.com>
2025-05-30 16:28:24 +0530

    feat: custom canvas form submission

--- app/(routes)/workspace/_components/CustomCanvasDialog.jsx
+++ app/(routes)/workspace/_components/CustomCanvasDialog.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -8,8 +10,41 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
+import { UserDetailContext } from "@/context/UserDetailContext";
+import { api } from "@/convex/_generated/api";
+import { IconLoader } from "@tabler/icons-react";
+import { useMutation } from "convex/react";
+import { useContext, useState } from "react";
+import { toast } from "sonner";
 
 const CustomCanvasDialog = ({ children }) => {
+  const [name, setName] = useState();
+  const [width, setWidth] = useState();
+  const [height, setHeight] = useState();
+
+  const [loading, setLoading] = useState(false);
+  const { userDetail } = useContext(UserDetailContext);
+  const createDesignRecord = useMutation(api.designs.CreateNewDesign);
+
+  const onCreate = async () => {
+    toast("Creating new design...");
+    setLoading(true);
+    const result = await createDesignRecord({
+      name: name,
+      width: +width,
+      height: +height,
+      uid: userDetail?._id,
+    });
+    setLoading(false);
+    resetForm();
+  };
+
+  const resetForm = () => {
+    setName(null);
+    setWidth(null);
+    setHeight(null);
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
@@ -26,20 +61,40 @@ const CustomCanvasDialog = ({ children }) => {
                     className="mt-1"
                     id="designName"
                     placeholder="Design Name"
+                    onChange={(e) => setName(e.target.value)}
                   />
                 </div>
                 <div className="flex gap-3.5">
                   <div className="flex flex-col">
                     <label htmlFor="width">Width</label>
-                    <Input className="mt-1" id="width" placeholder="500" />
+                    <Input
+                      className="mt-1"
+                      type="number"
+                      id="width"
+                      placeholder="500"
+                      onChange={(e) => setWidth(e.target.value)}
+                    />
                   </div>
                   <div className="flex flex-col">
                     <label htmlFor="height">Height</label>
-                    <Input className="mt-1" id="height" placeholder="500" />
+                    <Input
+                      className="mt-1"
+                      type="number"
+                      id="height"
+                      placeholder="500"
+                      onChange={(e) => setHeight(e.target.value)}
+                    />
                   </div>
                 </div>
                 <div className="flex justify-end">
-                  <Button className="cursor-pointer">Create</Button>
+                  <Button
+                    className="flex items-center justify-center cursor-pointer"
+                    disabled={loading || !name || !width || !height}
+                    onClick={onCreate}
+                  >
+                    {loading && <IconLoader className="mr-1 animate-spin" />}
+                    {loading ? "Creating..." : "Create"}
+                  </Button>
                 </div>
               </div>
             </div>
