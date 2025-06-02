7eeaa4c0ec193961ee724cd4e9281ff88e01cfe9
Viral Patel <viralpatel508@gmail.com>
2025-06-02 13:25:47 +0530

    feat: design header ui

--- app/(routes)/design/_components/DesignHeader.jsx
+++ app/(routes)/design/_components/DesignHeader.jsx
@@ -1,7 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 import { UserButton } from "@stackframe/stack";
 import {
+  IconArrowBackUp,
+  IconArrowForwardUp,
+  IconCloudCheck,
   IconDiamondFilled,
   IconMessage2,
   IconUpload,
@@ -11,7 +16,7 @@ import Image from "next/image";
 const DesignHeader = () => {
   return (
     <div className="p-2 px-5 flex items-center justify-between bg-linear-90 from-[#00c4cc] to-[#7d2ae8]">
-      <div>
+      <div className="flex items-center">
         <Image
           src={"/Canva_Logo.svg"}
           width={80}
@@ -19,11 +24,44 @@ const DesignHeader = () => {
           alt={"logo"}
           className="brightness-0 invert"
         />
+        <Separator
+          orientation="vertical"
+          className="mx-3 ml-5 min-h-8 opacity-30"
+        />
+        <div className="flex items-center">
+          <Button
+            size="icon"
+            variant="ghost"
+            className="cursor-pointer border-none text-white transition-colors hover:bg-white/5 hover:text-white"
+          >
+            <IconArrowBackUp strokeWidth="1.75" className="size-6" />
+          </Button>
+          <Button
+            size="icon"
+            variant="ghost"
+            className="ml-3 cursor-pointer border-none text-white transition-colors hover:bg-white/5 hover:text-white"
+          >
+            <IconArrowForwardUp strokeWidth="1.75" className="size-6" />
+          </Button>
+        </div>
+        <Separator orientation="vertical" className="mx-3 min-h-8 opacity-30" />
+        <Tooltip>
+          <TooltipTrigger>
+            <Button
+              size="icon"
+              variant="ghost"
+              className="cursor-pointer border-none text-white transition-colors hover:bg-white/5 hover:text-white"
+            >
+              <IconCloudCheck strokeWidth="1.75" className="size-6" />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>All changes saved</TooltipContent>
+        </Tooltip>
       </div>
       <div className="flex items-center">
         <Input
           placeholder="Untitled design"
-          className="shadow-none placeholder-white/50 border-1 border-white/30 text-white mr-4"
+          className="shadow-none font-medium h-10 max-w-56 placeholder:text-white border-1 border-transparent text-white mr-4 transition-all focus-visible:ring-0 hover:border-white/30 focus:border-white/30"
         />
         <Button
           className="flex items-center mr-4 cursor-pointer border-1 border-white/30 text-white transition-colors hover:bg-white/5 hover:text-white"
@@ -39,14 +77,14 @@ const DesignHeader = () => {
           variant="ghost"
           className="ml-4 cursor-pointer border-1 border-white/30 text-white transition-colors hover:bg-white/5 hover:text-white"
         >
-          <IconMessage2 className="size-5" />
+          <IconMessage2 strokeWidth="1.75" className="size-5" />
         </Button>
         <Button
           className="flex items-center ml-4 cursor-pointer"
           size="lg"
           variant="outline"
         >
-          <IconUpload className="size-5" />
+          <IconUpload strokeWidth="1.75" className="size-5" />
           Share
         </Button>
       </div>
